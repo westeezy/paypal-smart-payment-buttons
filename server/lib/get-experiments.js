74887d63a64da642cbb64ef4e7710e8076b06e7e
fedperez <fedperez@paypal.com>
2021-12-07 21:42:59 -0300

    get experiments type for new props

--- server/lib/get-experiments.js
+++ server/lib/get-experiments.js
@@ -5,10 +5,16 @@ import { FUNDING } from '@paypal/sdk-constants';
 import type { Wallet } from '../../src/types';
 
 export type GetExperimentsParams = {|
+    merchantID : string,
     buttonSessionID : string,
     clientID : string,
     fundingSource : ?$Values<typeof FUNDING>,
-    wallet : Wallet
+    wallet : Wallet,
+    locale : {|
+        lang : string,
+        country : string
+    |},
+    buyerCountry : string
 |};
 
 export type GetExperimentsType = {|
